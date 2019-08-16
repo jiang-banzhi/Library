9ef05835e55f855ac532b6aa25cf1892e7819ed8
banzhi <jy494495991@gmail.com>
2019-08-16 15:21:35 +0800

    增加toolbar title居中 删除无用方法

--- app/src/main/java/com/banzhi/sample/MainActivity.java
+++ app/src/main/java/com/banzhi/sample/MainActivity.java
@@ -1,7 +1,12 @@
 package com.banzhi.sample;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.banzhi.lib.base.IBaseActivity;
 import com.banzhi.lib.utils.BarUtils;
@@ -16,6 +21,11 @@ public class MainActivity extends IBaseActivity {
     }
 
     @Override
+    protected boolean hasToolbarLayout() {
+        return true;
+    }
+
+    @Override
     protected int getLayoutId() {
 
         return R.layout.activity_main;
@@ -23,7 +33,24 @@ public class MainActivity extends IBaseActivity {
 
     @Override
     protected void initView(Bundle savedInstanceState) {
+        mToolbar.setNavigationIcon(R.mipmap.ic_launcher);
+
+        TextView textView = findView(R.id.text);
     }
+
+
+
+    @Override
+    public void setTitle(int resId) {
+        super.setTitle(resId);
+    }
+
+    @Override
+    protected boolean isTitleCenter() {
+        return true;
+    }
+
+
 
     @Override
     protected void initListener() {
@@ -31,10 +58,38 @@ public class MainActivity extends IBaseActivity {
 
     @Override
     protected void initData() {
-        BarUtils.setStatusBarColor(this,getResources().getColor(R.color.colorPrimary),0);
+        BarUtils.setStatusBarColor(this, getResources().getColor(R.color.colorPrimary), 0);
     }
 
     @Override
     protected void processClick(View v) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.test, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        TextView textView = findView(R.id.text);
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) textView.getLayoutParams();
+        switch (item.getItemId()) {
+            case R.id.menu1:
+                layoutParams.gravity = Gravity.LEFT;
+                setTitle("item1");
+                break;
+            case R.id.menu2:
+                layoutParams.gravity = Gravity.RIGHT;
+                setTitle("item2");
+                break;
+            default:
+
+                break;
+        }
+
+        textView.setLayoutParams(layoutParams);
+        return super.onOptionsItemSelected(item);
     }
 }
